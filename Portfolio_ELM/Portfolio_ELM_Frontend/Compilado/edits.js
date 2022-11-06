9ddb412605e434ad16bf307ba78c9f29e19d237b
Oli <theo@frika.com.ar>
2022-11-06 20:41:26 -0300

    intentos

---
+++ Portfolio_ELM/Portfolio_ELM_Frontend/Compilado/edits.js
@@ -0,0 +1,10 @@
+
+    var contact_edit = document.querySelector(".contact_edit");
+    var btn_edit_contact = document.getElementById("btn_edit_contact");
+
+    btn_edit_contact.addEventListener("click", Contact_edit);
+
+    function Contact_edit() {
+        contact_edit.classList.add("contact_edit");
+    }
+
---
+++ Portfolio_ELM/Portfolio_ELM_Frontend/src/edits.ts
@@ -0,0 +1,9 @@
+(()=>{
+    const contact_edit = document.querySelector(".contact_edit");
+
+    function Contacto_edit(){
+        contact_edit.classList.toggle("contact_edit");
+     }
+    
+
+})();
