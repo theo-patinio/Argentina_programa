ff9f9ba7f45b9027a42d06479092d13abbeeeb6e
Theo Patinio <theo@frika.com.ar>
2022-11-07 09:03:16 -0300

    vamos avanzando con edit

--- Portfolio_ELM/Portfolio_ELM_Frontend/src/edits.ts
+++ Portfolio_ELM/Portfolio_ELM_Frontend/src/edits.ts
@@ -1,9 +1,31 @@
 (()=>{
-    const contact_edit = document.querySelector(".contact_edit");
+  let contact_edit = document.querySelector(".contact_edit");
+  let btn_edit_contact = document.getElementById("btn_edit_contact");
+  let acerca_edit = document.querySelector(".acerca_edit");
+  let btn_edit_acerca = document.getElementById("btn_edit_acerca");
+  let btn_enviar_text = document.getElementById("btn_enviar_text");
+  let acerca_text = document.getElementById("acerca_text")
+  let acerca = document.getElementById("acerca")
 
-    function Contacto_edit(){
-        contact_edit.classList.toggle("contact_edit");
-     }
-    
+
+   btn_edit_contact.addEventListener("click", mostrar_edit_contact);
+   btn_edit_acerca.addEventListener("click", mostrar_edit_acerca);
+   btn_enviar_text.addEventListener("click", editar_text_acerca);
+
+   function mostrar_edit_contact() {
+       contact_edit.classList.toggle("contact_edit");
+       console.log("boton edicio")
+    }
+   function mostrar_edit_acerca() {
+    acerca_edit.classList.toggle("acerca_edit");
+    console.log("boton acerca")
+    }
+
+    function editar_text_acerca() {
+      acerca.innerText = "hola";
+    }
+
+
+
 
 })();
